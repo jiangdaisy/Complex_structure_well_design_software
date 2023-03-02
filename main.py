ed8ae8aa20d06191db1073be54da1bba7de38829
Jiang Ang <2548187267@qq.com>
2023-03-02 22:05:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from CSw_sjk import *
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
